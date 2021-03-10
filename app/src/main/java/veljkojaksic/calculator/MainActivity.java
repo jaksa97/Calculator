78e3c6dec9b66d098c0357f9a2a38d615345e720
jaksa97 <veljko.jaksic@pmf.edu.rs>
2021-03-10 10:17:03 +0100

    Add OnClickListener except for Backspace and Equals buttons

--- app/src/main/java/veljkojaksic/calculator/MainActivity.java
+++ app/src/main/java/veljkojaksic/calculator/MainActivity.java
@@ -29,58 +29,82 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void updateText(String strToAdd){
+        display.setText(String.format("%s%s", display.getText().toString(), strToAdd));
+        display.setSelection(display.getSelectionStart() + 1);
+    }
+
     public void btnClearClick(View view) {
+        display.setText("");
+        previousCalculation.setText("");
     }
 
     public void btnParenthesesOpenClick(View view) {
+        updateText(getResources().getString(R.string.btnParenthesesOpenText));
     }
 
     public void btnParenthesesCloseClick(View view) {
+        updateText(getResources().getString(R.string.btnParenthesesCloseText));
     }
 
     public void btnDivideClick(View view) {
+        updateText(getResources().getString(R.string.btnDivideText));
     }
 
     public void btnSevenClick(View view) {
+        updateText(getResources().getString(R.string.btnSevenText));
     }
 
     public void btnEightClick(View view) {
+        updateText(getResources().getString(R.string.btnEightText));
     }
 
     public void btnNineClick(View view) {
+        updateText(getResources().getString(R.string.btnNineText));
     }
 
     public void btnMultiplyClick(View view) {
+        updateText(getResources().getString(R.string.btnMultiplyText));
     }
 
     public void btnFourClick(View view) {
+        updateText(getResources().getString(R.string.btnFourText));
     }
 
     public void btnFiveClick(View view) {
+        updateText(getResources().getString(R.string.btnFiveText));
     }
 
     public void btnSixClick(View view) {
+        updateText(getResources().getString(R.string.btnSixText));
     }
 
     public void btnSubtractClick(View view) {
+        updateText(getResources().getString(R.string.btnSubtractText));
     }
 
     public void btnOneClick(View view) {
+        updateText(getResources().getString(R.string.btnOneText));
     }
 
     public void btnTwoClick(View view) {
+        updateText(getResources().getString(R.string.btnTwoText));
     }
 
     public void btnThreeClick(View view) {
+        updateText(getResources().getString(R.string.btnThreeText));
     }
 
     public void btnAddClick(View view) {
+        updateText(getResources().getString(R.string.btnAddText));
     }
 
     public void btnZeroClick(View view) {
+        updateText(getResources().getString(R.string.btnZeroText));
     }
 
     public void btnDecimalClick(View view) {
+        updateText(getResources().getString(R.string.btnDecimalText));
     }
 
     public void btnEqualClick(View view) {
