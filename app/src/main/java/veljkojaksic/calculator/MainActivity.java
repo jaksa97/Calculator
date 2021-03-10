2e598b19a74bb76075b9dbc1ac9aebf4d46d3d7a
jaksa97 <veljko.jaksic@pmf.edu.rs>
2021-03-10 09:23:20 +0100

    Portrait layout

--- app/src/main/java/veljkojaksic/calculator/MainActivity.java
+++ app/src/main/java/veljkojaksic/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package veljkojaksic.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnClearClick(View view) {
+    }
+
+    public void btnParenthesesOpenClick(View view) {
+    }
+
+    public void btnParenthesesCloseClick(View view) {
+    }
+
+    public void btnDivideClick(View view) {
+    }
+
+    public void btnSevenClick(View view) {
+    }
+
+    public void btnEightClick(View view) {
+    }
+
+    public void btnNineClick(View view) {
+    }
+
+    public void btnMultiplyClick(View view) {
+    }
+
+    public void btnFourClick(View view) {
+    }
+
+    public void btnFiveClick(View view) {
+    }
+
+    public void btnSixClick(View view) {
+    }
+
+    public void btnSubtractClick(View view) {
+    }
+
+    public void btnOneClick(View view) {
+    }
+
+    public void btnTwoClick(View view) {
+    }
+
+    public void btnThreeClick(View view) {
+    }
+
+    public void btnAddClick(View view) {
+    }
+
+    public void btnZeroClick(View view) {
+    }
+
+    public void btnDecimalClick(View view) {
+    }
+
+    public void btnEqualClick(View view) {
+    }
+
+    public void btnBackspaceClick(View view) {
     }
 }
